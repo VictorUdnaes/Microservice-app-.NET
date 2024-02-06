abb151423e93efcb7baed1c6ab2479d1d6738913
Victor Udnæs <udnaesv@gmail.com>
2024-02-06 17:16:39 +0100

    Initialized postgres database with some values.

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -5,9 +5,9 @@ namespace AuctionService.Entities;
 public class Auction
 {
     public Guid Id { get; set; }
-    public int ReservePrice { get; set; } = 0;
+    public int ReservePrice { get; set; }
     public string Seller { get; set; }
-    public string winner { get; set; }
+    public string? Winner { get; set; }
     public int? SoldAmount { get; set; }
     public int? CurrentHighBid { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
